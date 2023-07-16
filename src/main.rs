967b0ad523b61230761aba129eb4665db3c1ed2b
skarfie123 <rahulpai@rocketmail.com>
2023-07-16 19:12:37 +0100

    better scale

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,42 @@ use nalgebra::{Complex, Normed};
 
 const MAX_ITER: i32 = 360;
 
-fn julia(c: Complex<f64>, x: f64, y: f64) -> i32 {
+fn julia(c: Complex<f64>, x: f64, y: f64) -> f64 {
     let mut z = Complex::new(x, y);
 
     for i in 0..MAX_ITER {
         if z.norm() > 2.0 {
-            return i;
+            return i as f64 / MAX_ITER as f64;
         }
 
         z = z * z + c;
     }
 
-    -1
+    -1.0
 }
 
 fn main() {
-    let width = 1980;
+    let width = 1920;
     let height = 1080;
+    let scale = 3.0;
 
-    let scale_x = 3.0 / width as f64;
-    let scale_y = 2.0 / height as f64;
+    let aspect = width as f64 / height as f64;
 
     let mut img = ImageBuffer::new(width, height);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let cx = x as f64 * scale_x - 1.5;
-        let cy = y as f64 * scale_y - 1.0;
+        let cx = (x as f64 / width as f64 - 0.5) * scale;
+        let cy = (y as f64 / height as f64 - 0.5) * scale / aspect;
 
         let c = Complex::new(-0.8, 0.156);
         let value = julia(c, cx, cy);
 
-        if value == -1 {
-            *pixel = Rgb([255, 255, 255]);
+        if value == -1.0 {
+            *pixel = Rgb([0, 0, 0]);
             continue;
         }
 
-        let (r, g, b) = hsv_to_rgb(
-            360.0 * value as f64 / MAX_ITER as f64,
-            1.0,
-            (value as f64 / MAX_ITER as f64).sqrt(),
-        );
+        let (r, g, b) = hsv_to_rgb(360.0 * value, 1.0, value.sqrt());
 
         *pixel = Rgb([r, g, b]);
     }
