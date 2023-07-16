7aa15f8afaafea73ab72c8638f489a7aeb13e52d
skarfie123 <rahulpai@rocketmail.com>
2023-07-16 22:12:29 +0100

    animate with progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use hsv::hsv_to_rgb;
 use image::{ImageBuffer, Rgb};
+use indicatif::ProgressIterator;
 use nalgebra::{Complex, Normed};
 use std::time::Instant;
 
-const MAX_ITER: i32 = 360;
+const MAX_ITER: i32 = 16;
 
-fn julia(c: Complex<f64>, x: f64, y: f64) -> f64 {
+fn julia(c: Complex<f64>, x: f64, y: f64, max_iter: i32) -> f64 {
     let mut z = Complex::new(x, y);
 
-    for i in 0..MAX_ITER {
+    for i in 0..max_iter {
         if z.norm() > 2.0 {
-            return i as f64 / MAX_ITER as f64;
+            return i as f64 / max_iter as f64;
         }
 
         z = z * z + c;
@@ -19,47 +20,40 @@ fn julia(c: Complex<f64>, x: f64, y: f64) -> f64 {
     -1.0
 }
 
-fn main() {
-    let now = Instant::now();
-    let width = 1920;
-    let height = 1080;
+fn generate_frame(max_iter: i32) {
+    let width = 1920 / 2;
+    let height = 1080 / 2;
     let scale = 3.0;
 
     let aspect = width as f64 / height as f64;
 
     let mut img = ImageBuffer::new(width, height);
 
-    let mut max = 0.0;
-
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let cx = (x as f64 / width as f64 - 0.5) * scale;
         let cy = (y as f64 / height as f64 - 0.5) * scale / aspect;
 
         let c = Complex::new(-0.8, 0.156);
-        let value = julia(c, cx, cy);
+        let value = julia(c, cx, cy, max_iter);
 
         if value == -1.0 {
             *pixel = Rgb([0, 0, 0]);
             continue;
         }
 
-        if value > max {
-            max = value;
-        }
-
         let (r, g, b) = hsv_to_rgb(360.0 * value, 1.0, value.powf(0.25));
 
         *pixel = Rgb([r, g, b]);
     }
-    println!("Max: {}", max * MAX_ITER as f64);
+
+    img.save(format!("julia\\{}.png", max_iter)).unwrap();
+}
+
+fn main() {
+    let now = Instant::now();
+    for i in (0..MAX_ITER).progress() {
+        generate_frame(i);
+    }
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
-
-    let result = img.save("julia.png");
-    match result {
-        Ok(()) => (),
-        Err(e) => eprintln!("{}", e),
-    }
-    let elapsed2 = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed2);
 }
