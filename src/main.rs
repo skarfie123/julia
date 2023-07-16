44557331831c6e66af1687d7ecadaa1d7b046df4
skarfie123 <rahulpai@rocketmail.com>
2023-07-16 21:59:32 +0100

    timing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use hsv::hsv_to_rgb;
 use image::{ImageBuffer, Rgb};
 use nalgebra::{Complex, Normed};
+use std::time::Instant;
 
 const MAX_ITER: i32 = 360;
 
@@ -19,6 +20,7 @@ fn julia(c: Complex<f64>, x: f64, y: f64) -> f64 {
 }
 
 fn main() {
+    let now = Instant::now();
     let width = 1920;
     let height = 1080;
     let scale = 3.0;
@@ -26,6 +28,8 @@ fn main() {
     let aspect = width as f64 / height as f64;
 
     let mut img = ImageBuffer::new(width, height);
+
+    let mut max = 0.0;
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let cx = (x as f64 / width as f64 - 0.5) * scale;
@@ -39,14 +43,23 @@ fn main() {
             continue;
         }
 
-        let (r, g, b) = hsv_to_rgb(360.0 * value, 1.0, value.sqrt());
+        if value > max {
+            max = value;
+        }
+
+        let (r, g, b) = hsv_to_rgb(360.0 * value, 1.0, value.powf(0.25));
 
         *pixel = Rgb([r, g, b]);
     }
+    println!("Max: {}", max * MAX_ITER as f64);
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 
     let result = img.save("julia.png");
     match result {
         Ok(()) => (),
         Err(e) => eprintln!("{}", e),
     }
+    let elapsed2 = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed2);
 }
