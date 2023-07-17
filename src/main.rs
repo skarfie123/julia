836af719af3b8347850772d33cbece4fbb531381
skarfie123 <rahulpai@rocketmail.com>
2023-07-17 22:09:40 +0100

    refactor scaling inside

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ const MAX_ITER: i32 = 36;
 const WIDTH: u32 = 1920;
 const HEIGHT: u32 = 1080;
 const SCALE: f64 = 3.0;
+const C: Complex<f64> = Complex::new(-0.8, 0.156);
 
 const ASPECT: f64 = WIDTH as f64 / HEIGHT as f64;
 
-fn julia(c: Complex<f64>, x: f64, y: f64, max_iter: i32) -> f64 {
-    let mut z = Complex::new(x, y);
+fn julia(c: Complex<f64>, x: u32, y: u32, max_iter: i32) -> f64 {
+    let scaled_x = (x as f64 / WIDTH as f64 - 0.5) * SCALE;
+    let scaled_y = (y as f64 / HEIGHT as f64 - 0.5) * SCALE / ASPECT;
+    let mut z = Complex::new(scaled_x, scaled_y);
 
     for i in 0..max_iter {
         if z.norm() > 2.0 {
@@ -30,11 +33,7 @@ fn generate_frame(max_iter: i32) {
     let mut img = ImageBuffer::new(WIDTH, HEIGHT);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let cx = (x as f64 / WIDTH as f64 - 0.5) * SCALE;
-        let cy = (y as f64 / HEIGHT as f64 - 0.5) * SCALE / ASPECT;
-
-        let c = Complex::new(-0.8, 0.156);
-        let value = julia(c, cx, cy, max_iter);
+        let value = julia(C, x, y, max_iter);
 
         if value == -1.0 {
             *pixel = Rgb([0, 0, 0]);
@@ -58,21 +57,29 @@ fn generate_frames(frames: Vec<i32>, pb: &ProgressBar) {
 
 fn main() {
     let now = Instant::now();
+
     let m = MultiProgress::new();
+
     let frames = 0..MAX_ITER;
+
     let num_threads = thread::available_parallelism().unwrap().get() as i32 - 1;
     let mut threads: Vec<thread::JoinHandle<()>> = vec![];
+
     for ti in 0..num_threads {
         let group: Vec<i32> = frames
             .clone()
             .filter(move |i| i % num_threads == ti)
             .collect();
+
         let pb = m.add(ProgressBar::new(group.len() as u64));
+
         let t = thread::spawn(move || generate_frames(group, &pb));
         threads.push(t);
     }
+
     for t in threads {
         t.join().unwrap();
     }
+
     println!("Elapsed: {:.2?}", now.elapsed());
 }
