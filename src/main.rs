3741f0083fb436213e8d747434d4b98498a66fb8
skarfie123 <rahulpai@rocketmail.com>
2023-07-18 23:14:33 +0100

    generate last frame first

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use indicatif::{MultiProgress, ProgressBar};
 use nalgebra::{Complex, DMatrix, Normed};
 use std::fs::File;
 use std::io::Write;
+use std::sync::Arc;
 use std::thread;
 use std::time::Instant;
 
@@ -16,26 +17,18 @@ const SCALE: f64 = 3.0;
 
 const C: Complex<f64> = Complex::new(-0.8, 0.156);
 
-type Cache = DMatrix<i32>;
+type Julia = DMatrix<i32>;
 
 const TIMINGS_FILE: &str = "timings.csv";
 type Timings = Vec<(i32, f32)>;
 
-fn julia(c: Complex<f64>, x: u32, y: u32, max_iter: i32, cache: &mut Cache) -> i32 {
-    let cached_value = cache[(x as usize, y as usize)];
-    if cached_value > 0 {
-        // make sure it's not from a future frame
-        if cached_value < max_iter {
-            return cached_value;
-        }
-    }
+fn julia(c: Complex<f64>, x: u32, y: u32) -> i32 {
     let scaled_x = (x as f64 / WIDTH as f64 - 0.5) * SCALE;
     let scaled_y = (y as f64 / HEIGHT as f64 - 0.5) * SCALE / ASPECT;
     let mut z = Complex::new(scaled_x, scaled_y);
 
-    for i in 0..max_iter {
+    for i in 0..MAX_ITER {
         if z.norm() > 2.0 {
-            cache[(x as usize, y as usize)] = i;
             return i;
         }
 
@@ -45,13 +38,32 @@ fn julia(c: Complex<f64>, x: u32, y: u32, max_iter: i32, cache: &mut Cache) -> i
     -1
 }
 
-fn generate_frame(max_iter: i32, cache: &mut Cache) {
+fn generate_julias(m: &MultiProgress) -> Julia {
+    let pb_x = m.add(ProgressBar::new(WIDTH as u64));
+    let pb_y = m.add(ProgressBar::new(HEIGHT as u64));
+
+    let mut data: Julia = DMatrix::<i32>::from_element(WIDTH as usize, HEIGHT as usize, -1);
+
+    for x in 0..WIDTH {
+        pb_y.set_position(0);
+        for y in 0..HEIGHT {
+            let value = julia(C, x, y);
+            data[(x as usize, y as usize)] = value;
+            pb_y.inc(1);
+        }
+        pb_x.inc(1);
+    }
+
+    data
+}
+
+fn generate_frame(max_iter: i32, data: &Julia) {
     let mut img = ImageBuffer::new(WIDTH, HEIGHT);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let value = julia(C, x, y, max_iter, cache);
+        let value = data[(x as usize, y as usize)];
 
-        if value == -1 {
+        if value == -1 || value > max_iter {
             *pixel = Rgb([0, 0, 0]);
             continue;
         }
@@ -65,12 +77,11 @@ fn generate_frame(max_iter: i32, cache: &mut Cache) {
     img.save(format!("julia\\{}.png", max_iter)).unwrap();
 }
 
-fn generate_frames(frames: Vec<i32>, pb: &ProgressBar) -> Timings {
-    let mut cache: Cache = DMatrix::<i32>::from_element(WIDTH as usize, HEIGHT as usize, -1);
+fn generate_frames(frames: Vec<i32>, pb: &ProgressBar, data: &Julia) -> Timings {
     let mut timings: Timings = vec![];
     for max_iter in frames {
         let now = Instant::now();
-        generate_frame(max_iter, &mut cache);
+        generate_frame(max_iter, data);
         timings.push((max_iter, now.elapsed().as_secs_f32()));
         pb.inc(1);
     }
@@ -84,10 +95,13 @@ fn main() {
 
     let frames = 0..MAX_ITER;
 
+    let data: Arc<Julia> = Arc::new(generate_julias(&m));
+
     let num_threads = thread::available_parallelism().unwrap().get() as i32 - 1;
     let mut threads: Vec<thread::JoinHandle<Timings>> = vec![];
 
     for ti in 0..num_threads {
+        let data = data.clone();
         let group: Vec<i32> = frames
             .clone()
             .filter(move |i| i % num_threads == ti)
@@ -95,7 +109,7 @@ fn main() {
 
         let pb = m.add(ProgressBar::new(group.len() as u64));
 
-        let t = thread::spawn(move || generate_frames(group, &pb));
+        let t = thread::spawn(move || generate_frames(group, &pb, &data));
         threads.push(t);
     }
 
