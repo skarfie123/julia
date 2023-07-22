09799a2f8c762edd68d21f35c599b57d8b0e9536
skarfie123 <rahulpai@rocketmail.com>
2023-07-22 21:22:59 +0100

    generate videos

---
+++ video.ps1
@@ -0,0 +1,19 @@
+﻿param (
+    [string]
+    $folder,
+
+    [string]
+    $inputType = 'bmp'
+)
+
+if (-not (Test-Path $folder -PathType Container)) {
+    Write-Output "$folder is not a folder!"
+    return
+}
+
+$inputPath = Join-Path $folder "%d.$inputType"
+$folderName = Split-Path -Path $folder -Leaf
+$outputPath = Join-Path output "$folderName.mp4"
+
+
+ffmpeg -i "$inputPath" -r 24 -c:v libx264 -preset slow -crf 18 $outputPath
