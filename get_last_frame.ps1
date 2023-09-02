1230e55e8c10c0c66a6b47fb9736b0b97b4158ae
skarfie123 <rahulpai@rocketmail.com>
2023-09-02 22:17:21 +0100

    get last frame

---
+++ get_last_frame.ps1
@@ -0,0 +1,18 @@
+﻿param (
+    [string]
+    $folder,
+
+    [string]
+    $inputType = 'bmp'
+)
+
+if (-not (Test-Path $folder -PathType Container)) {
+    Write-Output "$folder is not a folder!"
+    return
+}
+
+$folderName = Split-Path -Path $folder -Leaf
+$outputPath = Join-Path output "${folderName}_last.bmp"
+
+
+Get-ChildItem -Path $folder -Filter *.bmp | Sort-Object LastWriteTime -Descending | Select-Object -First 1 | ForEach-Object { Copy-Item $_ $outputPath }
